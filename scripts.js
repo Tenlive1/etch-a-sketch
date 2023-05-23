54e3447e90914604d7bdcd783c5315e3ceb35583
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-05-23 01:54:50 -0500

    got the click and drag working but for some reason it keep on thinking im constatly dragging

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ function creategrid(rows,cols){
     for(let x=0; x<rows; x++){
         let row = document.createElement('div');
         row.classList.add("row");
+        row.id = x;
 
         for(let y = 0; y<cols; y++){
             let col = document.createElement('div');
@@ -20,11 +21,25 @@ function creategrid(rows,cols){
 }
 creategrid(16,16);
 
-const buttons = document.querySelectorAll('.col');
 
-buttons.forEach((div) =>{
-    div.addEventListener('click',() =>{
-        alert(div.id);
-    })
+const cols = document.querySelectorAll('.col');
+
+cols.forEach((div) =>{
+    let held =false;
+    div.addEventListener('mousedown',()=>{
+        held =true;
+        div.style.backgroundColor = "black";
+        cols.forEach((test) => {
+            if(held){
+                test.addEventListener('mouseover',()=>{
+                    test.style.backgroundColor = "black";
+                });
+            }
+        });
+    });
+    div.addEventListener('mouseup',()=>{
+        held = false;
+        console.log(held);
+    });
 })
 
